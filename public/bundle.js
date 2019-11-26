1ac713cbab7eb3ac0694bcba60458546a3171b1b
Joan <Joanpadolina@gmail.com>
2019-11-26 21:48:37 +0100

    i can log the selected in option
    but im still figuring out how to change the barchart with that option

--- public/bundle.js
+++ public/bundle.js
@@ -2,18 +2,18 @@
     'use strict';
 
     let data = [{
-            "category": "foto",
-            "continent": "Azië",
-            "value": 68656
+            'category': 'foto',
+            'continent': 'Azië',
+            'value': 68656
         },
         {
-            "category": "foto",
-            "continent": "Azië",
-            "value": 23763
+            'category': 'foto',
+            'continent': 'Azië',
+            'value': 23763
         },
         {
-            "category": "negatief",
-            "continent": "Azië",
+            'category': 'negatief',
+            'continent': "Azië",
             "value": 17367
         },
         {
@@ -263,11 +263,6 @@
         }
     ];
 
-    function merge(data) {
-        data.map(d => d.category.toLowerCase());
-    }
-    merge(data);
-
 
     function colorList() {
         return d3.scaleOrdinal(["#29A567", "#586BA4", "#ED4D6E", "#FAFFD8", "#AED9E0", "#FECCBA"])
@@ -281,14 +276,15 @@
     const svg = d3.select("svg"),
         margin = {
             top: 20,
-            right: 20,
+            right: 50,
             bottom: 30,
-            left: 40
+            left: 100
         },
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // x en y as wordt gemaakt
     let x0 = d3.scaleBand()
         .rangeRound([0, width])
         .paddingInner(0.2);
@@ -313,7 +309,6 @@
         .entries(data);
 
     // hier verander ik de namen van de data zodat ik er een beter overzicht heb dan keys gebruiken
-
     nestedCategory.forEach(i => {
         i.category = i.key;
         delete i.key;
@@ -330,15 +325,12 @@
 
     });
 
+    // geeft een naam voor bepaalde waardes om dit makkelijker te achterhalen
     let continentList = nestedCategory[0].values.map(d => d.continent).splice(0, 5); // laatste van de continent verwijderd ivm spelling.
     let categoryList = nestedCategory.map(d => d.category);
 
-    // tool tip in de html plaatsen
 
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
+    // hier geef ik de x en y as hun eigen waardes
     x0.domain(categoryList);
     x1.domain(continentList).rangeRound([0, x0.bandwidth()]);
     y.domain([0, d3.max(nestedCategory, d => {
@@ -357,10 +349,9 @@
         .append('g')
         .attr('transform', d => {
             return `translate(${x0(d.category)},0)`
-        })
-        .attr('class', 'category-group');
+        });
 
-
+    // de rectangles worden hier gemaakt
     const rect = category.selectAll('rect')
         .data(d => (d.values))
         .enter()
@@ -370,9 +361,7 @@
         .attr('width', x0.bandwidth())
         .attr('height', 0);
 
-
-    // console.log(nestedCategory)
-
+    // de chart begint met een groep per category
     let groupBar = () => {
         rect.transition()
             .duration(500)
@@ -384,11 +373,10 @@
             .attr('witdh', x0.bandwidth())
             .attr('height', d => height - y(d.totalValue))
             .attr('fill', d => z(d.continent));
-
-
     };
-    let drawLegend = (data) => {
 
+    // legenda uit de data
+    let continentLegend = (data) => {
         var legend = g.append("g")
             .attr("font-family", "sans-serif")
             .attr("font-size", 10)
@@ -398,12 +386,12 @@
             .enter()
             .append("g")
             .attr("transform", (d, i) => {
-                return "translate(0," + i * 20 + ")";
+                return "translate(0," + i * 25 + ")";
             });
 
         legend.append("rect")
             .attr("x", width - 19)
-            .attr("width", 19)
+            .attr("width", 10)
             .attr("height", 19)
             .attr("fill", z);
 
@@ -411,6 +399,7 @@
             .attr("x", width - 24)
             .attr("y", 9.5)
             .attr("dy", "0.32em")
+            .attr('fill', 'darkgray')
             .text(d => {
                 return d;
             });
@@ -422,42 +411,47 @@
     // let leftAxis = d3.axisLeft(y)
     //     .tickFormat(d => Math.round(d * 100 / d3.max(y.domain())) + "%")
 
+    //Axis voor de chart
     let drawAxis = () => {
-        g.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + height + ")")
+        g.append('g')
+            .attr('class', 'xaxis')
+            .attr('transform', 'translate(0,' + height + ')')
             .call(d3.axisBottom(x0));
 
-        g.append("g")
-            .attr("class", "axis")
+        g.append('g')
+            .attr('class', 'yaxis')
             .call(d3.axisLeft(y))
-            .append("text")
-            .attr("x", 2)
-            .attr("y", y(y.ticks().pop()) + 0.5)
-            .attr("dy", "0.32em")
-            .attr("fill", "#000")
-            .attr("font-weight", "bold")
-            .attr("text-anchor", "start")
-            .text("aantal verzameld");
+            .append('text')
+            .attr('x', 2)
+            .attr('y', y(y.ticks().pop()) + 0.5)
+            .attr('dy', '1.32em')
+            .attr('fill', '#000')
+            .attr('font-weight', 'bold')
+            .attr('text-anchor', 'start')
+            .attr('transform', 'rotate(90)translate(140, 60)')
+            .text('aantal verzameld');
     };
 
-
+    // aanroepen van de functies
     groupBar();
-    drawLegend(continentList);
+    continentLegend(continentList);
     drawAxis();
 
 
-    d3.selectAll("input").on("change", change);
+    // van groep naar stacked
+    d3.selectAll('input').on('change', change);
 
 
     let timeout = setTimeout(() => {
-        d3.select("input[value=\"grouped\"]").property("checked", true).each(change);
+        d3.select('input[value=\'grouped\']').property('checked', true).each(change);
     }, 400);
+
 
     function change() {
         clearTimeout(timeout);
-        if (this.value === "grouped") transitionGrouped();
+        if (this.value === 'grouped') transitionGrouped();
         else transitionStacked();
+        console.log(this.value);
     }
 
     function transitionGrouped() {
@@ -467,15 +461,15 @@
             .delay((d, i) => {
                 return i * 10;
             })
-            .attr("x", d => {
+            .attr('x', d => {
                 return x1(d.continent);
             })
-            .attr("width", x1.bandwidth())
+            .attr('width', x1.bandwidth())
             .transition()
-            .attr("y", d => {
+            .attr('y', d => {
                 return y(d.totalValue);
             })
-            .attr("height", d => {
+            .attr('height', d => {
                 return height - y(d.totalValue);
             });
     }
@@ -487,67 +481,131 @@
             .delay((d, i) => {
                 return i * 10;
             })
-            .attr("y", d => {
+            .attr('y', d => {
                 return y(d.totalValue);
             })
-            .attr("height", d => {
+            .attr('height', d => {
                 return height - y(d.totalValue);
             })
             .transition()
-            .attr("x", d => {
+            .attr('x', d => {
                 return x0(d.continent);
             })
-            .attr("width", x0.bandwidth());
+            .attr('width', x0.bandwidth());
     }
 
     // hier maak ik een dropdown keuze menu van de continenten die in de database staan
     let dropdown = d3.select('body')
-        .append('select');
+        .append('select')
+        .attr('id', 'filter');
 
-    dropdown.selectAll('continent')
+    dropdown
+        .selectAll('option')
         .data(continentList)
         .enter()
         .append('option')
         .text(d => d)
         .attr('value', d => d);
 
+
+
+
+    // trying to update the data
+    function updateBars() {
+        console.log(data);
+
+        x1.domain(d3.extent(data, d => d.category));
+        y.domain([0, d3.max(data, d => d.totalValue)]);
+        let newDataGroup = d3.group( data => data.continent);
+        let selectNew = d3.selectAll('.bar').data(data);
+
+        selectNew
+            .enter()
+            .data(newDataGroup)
+            .attr('class', 'newBar')
+            .attr('x', (d,i) => x1(category[i]))
+            .attr('width', x1.rangeBand())
+            .attr('y', (d, i) => y(d))
+            .attr('height', (d, i) => height -y(d))
+            .attr('height', d => d + 'px');
+
+        selectNew.exit().remove();
+
+        updateBars();
+    }
+
+    function dataFil(nestedCategory) {
+        console.log(this.value);
+        console.log(data);
+        let updateData = d3.select(this).property('value');
+            newHeart = nestedCategory[updateBars];
+
+        updateBars(newHeart);
+        // if (this.value === 'value') transitionGrouped();
+        // else transitionStacked();
+        // let newD = d3.select('#filter').property('value')
+        // return d.continent === newD
+    }
+    // let initialData = nestedCategory[continent[0]]
+
+
+    d3.selectAll('#filter').on('change', dataFil);
+
+
+
+    // tool tip in de html plaatsen
+    let div = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+
+    //tooltip activeren
     category.selectAll('rect')
-        .on("mouseover", toolOn)
-        .on("mouseout", toolOf);
+        .on('mouseover', toolOn)
+        .on('mouseout', toolOf);
 
-
-
-    // function voor de tooltip
-    // https://bl.ocks.org/d3noob/257c360b3650b9f0a52dd8257d7a2d73
-
-
+    // wat er gebeurt als je over de rectagles overheen gaat
     function toolOn(d) {
         div.transition()
             .duration(200)
-            .style("opacity", .9);
+            .style('opacity', .9);
         div.html(`${d.continent} <br><hr> ${d.totalValue}`)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 20) + "px");
+            .style('left', (d3.event.pageX) + 'px')
+            .style('top', (d3.event.pageY - 20) + 'px');
         d3.select(this)
             .transition()
             .duration(300)
             .attr('fill', 'orange');
     }
 
+    // wat er gebeurt als je weer weg gaat met de muis
     function toolOf(d) {
         div.transition()
             .duration(500)
-            .style("opacity", 0);
+            .style('opacity', 0);
         d3.select(this)
             .transition()
             .duration(500)
             .attr('fill', d => z(d.continent));
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
     // console.log(data)
     //select all bars on the graph, take them out, and exit the previous data set. 
     //then you can add/enter the new data set
-    // var bars = chart.selectAll(".bar")
+    // var bars = chart.selectAll('.bar')
     //     .remove()
     //     .exit()
     //     .enter()
@@ -560,17 +618,17 @@
 
     // //now actually give each rectangle the corresponding data
     // bars.enter()
-    //     .append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", d => xScale(d.category)) // dit geeft de aantaal arrays terug en plaatst het op de xas
-    //     .attr("y", function (d) {
+    //     .append('rect')
+    //     .attr('class', 'bar')
+    //     .attr('x', d => xScale(d.category)) // dit geeft de aantaal arrays terug en plaatst het op de xas
+    //     .attr('y', function (d) {
     //         // console.log(d)
     //         return yScale(d.value);
     //     })
-    //     .attr("height", function (d) {
+    //     .attr('height', function (d) {
     //         return height - yScale(d.value);
     //     })
-    //     .attr("width", barWidth - 1)
+    //     .attr('width', barWidth - 1)
     //     .attr('fill', d => color(colorContinent(d)))
 
     // var xAxis = d3.axisBottom(xScale);
@@ -578,35 +636,35 @@
     // //left axis
     // //set up axes
     // //left axis
-    // chart.append("g")
-    //     .attr("class", "y axis")
+    // chart.append('g')
+    //     .attr('class', 'y axis')
     //     .call(yAxis)
     //     .selectAll('text')
 
 
     // //bottom axis
-    // chart.append("g")
-    //     .attr("class", "xAxis")
-    //     .attr("transform", "translate(0," + height + ")")
+    // chart.append('g')
+    //     .attr('class', 'xAxis')
+    //     .attr('transform', 'translate(0,' + height + ')')
     //     .call(xAxis)
-    //     .selectAll("text")
-    // // .style("text-anchor", "end")
-    // // .attr("dx", "-.8em")
-    // // .attr("dy", ".15em")
-    // // .attr("transform", function (d) {
-    // //     return "rotate(-65)";
+    //     .selectAll('text')
+    // // .style('text-anchor', 'end')
+    // // .attr('dx', '-.8em')
+    // // .attr('dy', '.15em')
+    // // .attr('transform', function (d) {
+    // //     return 'rotate(-65)';
     // // });
 
 
     // //add labels
     // chart
-    //     .append("text")
-    //     .attr("transform", "translate(-35," + (height + margin.bottom) / 2 + ") rotate(-90)")
-    //     .text("% of total watch time");
+    //     .append('text')
+    //     .attr('transform', 'translate(-35,' + (height + margin.bottom) / 2 + ') rotate(-90)')
+    //     .text('% of total watch time');
 
     // chart
-    //     .append("text")
-    //     .attr("transform", "translate(" + (width / 2) + "," + (height + margin.bottom - 5) + ")")
-    //     .text("categorie");
+    //     .append('text')
+    //     .attr('transform', 'translate(' + (width / 2) + ',' + (height + margin.bottom - 5) + ')')
+    //     .text('categorie');
 
 }());
